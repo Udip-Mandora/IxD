a3ce22660563a3878f8b83a64e1c1209be5f5bcf
Udip <udipmandora42@gmail.com>
2023-04-24 02:22:31 -0400

    adjusted output

--- script/script.js
+++ script/script.js
@@ -2,13 +2,14 @@ runSpeechRecog = () => {
     // document.getElementById("input").innerHTML = "Loading text...";
     var output = document.getElementById('input');
     var otp = document.getElementById('output');
+    var otpOne = document.getElementById('otp');
     let recognization = new webkitSpeechRecognition();
     recognization.onstart = () => {
-        otp.innerHTML = "Listening...";
+        otpOne.innerHTML = "Listening...";
     }
     recognization.onresult = (e) => {
         var transcript = e.results[0][0].transcript;
-        output.innerHTML = transcript;
+        otp.innerHTML = transcript;
         // output.classList.remove("hide")
         // action.innerHTML = "";
     }
