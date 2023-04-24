b47a82256062d8fe6a14b08e6f75633550748344
Udip <udipmandora42@gmail.com>
2023-04-24 02:15:33 -0400

    added css & changed javascript

--- script/script.js
+++ script/script.js
@@ -1,10 +1,11 @@
 runSpeechRecog = () => {
     // document.getElementById("input").innerHTML = "Loading text...";
     var output = document.getElementById('input');
+    var otp = document.getElementById('output');
     let recognization = new webkitSpeechRecognition();
-    // recognization.onstart = () => {
-    //     output.innerHTML = "Listening...";
-    // }
+    recognization.onstart = () => {
+        otp.innerHTML = "Listening...";
+    }
     recognization.onresult = (e) => {
         var transcript = e.results[0][0].transcript;
         output.innerHTML = transcript;
